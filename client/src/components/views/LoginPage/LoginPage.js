266ab3bd8c5d2f482d8507221e28250bc3544e75
Lee junseok <jun971006@gmail.com>
2020-10-26 20:49:16 +0900

    시행착오

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -40,10 +40,6 @@ export default function Loginpage(props) {
             display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
 
         }}>
-            
-
-        }}
-      
             <form style={{display:'flex', flexDirection: 'column'}}
                 onSubmit={onSubmitHandler}
             >
